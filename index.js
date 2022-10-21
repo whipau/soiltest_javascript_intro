7a5d9ef9d989f989ad7905f1ae3506b76a4bf428
Lizadel Yarisantos <63756348+deliscool@users.noreply.github.com>
2022-10-20 23:15:50 -0600

    update file location

--- index.js
+++ index.js
@@ -4,11 +4,14 @@
 //pH 0-7 is acidic
 //7-14 is basic
 // 7 is neutral
+
+
+// This function prompts user to 
 function sampleFunction() {
     var user = prompt("What pH level is your soil?");
     if (user <= 8) {
     document.getElementById("Soilcalculator").innerHTML =
-    "You can plant - vegetables, flowers or fruits";
+    "You can plant the following: vegetables, flowers or fruits";
     }
     else {
     document.getElementById("Soilcalculator").innerHTML =
